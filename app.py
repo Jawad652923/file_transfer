a77550a6561bd7b4c13087848da0ce05d9e8d1ea
Jawad652923 <jawadashraf548@gmail.com>
2023-09-01 22:28:52 +0500

    new changes occur

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask,render_template,request,send_from_directory
+from flask import Flask,render_template,request,send_file,redirect,url_for
 from werkzeug.utils import secure_filename
 app=Flask(__name__)
 
@@ -12,21 +12,34 @@ app.config['UPLOAD_FOLDER']= upload_folder
 
 
 
-@app.route("/")
+@app.route("/index")
 def index():
     return render_template('upload_file.html')
 
 @app.route('/upload', methods=['GET','POST'])
 def upload_file():
     if request.method=='POST':
-        file=request.files['file']        
+        file=request.files['file']   
+        file_name=request.args.get('file')    
+        print(file_name) 
         file.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
-        return f'file uploaded successfully'
+        return render_template('download_link.html',name=file.filename)
+    # return redirect(url_for('index'))
 
-@app.route('/download_link/<path:filename>', methods=['GET', 'POST'])
-def download_link(filename):
-   permitted_directory='/path/to/directoy/'
-   return send_from_directory(directory=permitted_directory, filename=filename,as_attachment=True,cache_timeout=0)
+
+
+# @app.route('/download/<filename>')
+# def download_link(filename):
+
+    
 
 if __name__=='__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
